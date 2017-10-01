6abd55530e0e6acc5008c4895fc1bc5d4448bad5
Maddie O'Meara <maddie.omeara@gmail.com>
2017-10-01 22:33:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*(100 + 1) + 1);
+  var itemObj = {[item]: price};
+  cart.push(itemObj);
+  console.log(`${item} has been added to your cart`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var cartContents = "In your cart you have";
+  var keys = Object.keys(cart)
+  if (!cart.length) {
+    cartContents = "Your shopping cart is empty.";
+  } else if (cart.length === 1) {
+      cartContents = ` ${keys[0]} at ${cart[0].keys}.`
+  } else if (cart.length ===2) {
+      cartContents = ` ${keys[0]} at ${cart[0].keys[0]} and ${keys[1]} at ${cart[1].keys[1]}.`
+  } else {
+      for (var i = 0; i < cart.length; i++) {
+        cartContents = `${cartContents} ${keys[i]} at ${cart[i].keys[i]}, and `;
+      }
+  }
+  return cartContents;
 }
 
 function total() {
